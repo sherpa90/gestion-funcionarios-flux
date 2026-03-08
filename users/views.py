04feec9b557cf667605a91efa19e9ad846c88648
sherpa90 <marco.rosasvillarroel@gmail.com>
2026-03-08 19:34:24 -0300

    se elimina paginacion en gestion de usuarios

--- users/views.py
+++ users/views.py
@@ -21,7 +21,7 @@ class UserListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
     model = CustomUser
     template_name = 'users/user_list.html'
     context_object_name = 'users'
-    paginate_by = 20  # Paginación: 20 usuarios por página
+    paginate_by = None  # Sin paginación - mostrar todos los usuarios
     
     def test_func(self):
         return self.request.user.role in ['SECRETARIA', 'ADMIN']
