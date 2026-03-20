c51e7513beaf314b3a6b6f87f0277666c2775a74
sherpa90 <marco.rosasvillarroel@gmail.com>
2026-03-20 13:09:59 -0300

    duplicidad axes

--- core/admin.py
+++ core/admin.py
@@ -1,33 +1,5 @@
 from django.contrib import admin
 
-# Intentar importar modelos de Axes
-try:
-    from axes.models import AccessAttempt, AccessLog
-    AXES_AVAILABLE = True
-except ImportError:
-    AXES_AVAILABLE = False
-
-
-if AXES_AVAILABLE:
-    @admin.register(AccessAttempt)
-    class AccessAttemptAdmin(admin.ModelAdmin):
-        list_display = ['username', 'ip_address', 'attempt_time', 'user_agent', 'locked']
-        list_filter = ['attempt_time', 'locked']
-        search_fields = ['username', 'ip_address']
-        readonly_fields = ['attempt_time', 'username', 'ip_address', 'user_agent', 'http_accept', 'path_info', 'locked']
-        ordering = ['-attempt_time']
-        
-        actions = ['desbloquear_seleccionados']
-        
-        def desbloquear_seleccionados(self, request, queryset):
-            queryset.delete()
-            self.message_user(request, 'Bloqueos seleccionados eliminados')
-        desbloquear_seleccionados.short_description = 'Desbloquear seleccionados'
-
-    @admin.register(AccessLog)
-    class AccessLogAdmin(admin.ModelAdmin):
-        list_display = ['username', 'ip_address', 'attempt_time', 'status']
-        list_filter = ['attempt_time', 'status']
-        search_fields = ['username', 'ip_address']
-        readonly_fields = ['attempt_time', 'username', 'ip_address', 'user_agent', 'status']
-        ordering = ['-attempt_time']
+# Nota: Los modelos de Axes (AccessAttempt, AccessLog) ya están registrados
+# automáticamente por el paquete axes en su propio admin.py
+# No es necesario registrarlos manualmente
