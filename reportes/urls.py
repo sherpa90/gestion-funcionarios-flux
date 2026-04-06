c109d3cb9549f731db137fa8c11f4cba6b5c2c81
sherpa90 <marco.rosasvillarroel@gmail.com>
2026-04-06 18:32:07 -0400

    mejoras visuales generales

--- reportes/urls.py
+++ reportes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ReportesView, PDFIndividualView, PDFColectivoView, ExportarExcelView, ReporteMensualDiasAdministrativosView, ExportarDAEMExcelView
+from .views import ReportesView, PDFIndividualView, PDFColectivoView, ExportarExcelView, ReporteMensualDiasAdministrativosView, ExportarDAEMExcelView, MiReportePDFView
 
 urlpatterns = [
     path('', ReportesView.as_view(), name='reportes'),
+    path('pdf/mi-reporte/', MiReportePDFView.as_view(), name='mi_reporte_pdf'),
     path('pdf/individual/<int:usuario_id>/', PDFIndividualView.as_view(), name='reportes_pdf_individual'),
     path('pdf/colectivo/', PDFColectivoView.as_view(), name='reportes_pdf_colectivo'),
     path('pdf/mensual/dias-administrativos/', ReporteMensualDiasAdministrativosView.as_view(), name='reportes_mensual_dias_administrativos'),
